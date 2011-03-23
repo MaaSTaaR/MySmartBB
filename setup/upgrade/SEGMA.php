b06cc7ad01486fd389490c2119f8b8c201d80a0b
Mohammed Q. Hussain <MaaSTaaR@gmail.com>
2011-03-23 12:10:24 +0000

    SEGMA's upgrader has been converted to the new upgrade system

--- setup/upgrade/SEGMA.php
+++ setup/upgrade/SEGMA.php
@@ -14,205 +14,101 @@ include('../common.php');
 
 require_once( 'MySmartUpgrader.class.php' );
 
-function sql_query( $sql )
-{
-	echo $sql;
-}
-
 class MySmartSEGMA extends MySmartInstall
 {
-	var $_TempArr 	= 	array();
-	var $_Masseges	=	array();
-	
-	// Convert old info table to new info table :)
-	/*function ConvertInfoTable()
-	{
-		global $MySmartBB;
-		
-		$prefix = $MySmartBB->getPrefix();
-		
-		$this->_TempArr['RenameArr'] = array();
-		$this->_TempArr['RenameArr']['old_name'] = $prefix . 'info';
-		$this->_TempArr['RenameArr']['new_name'] = $prefix . 'oldinfo';
-		
-		$rename = $this->rename_field($this->_TempArr['RenameArr']);
-	
-		if ($rename)
-		{
-			$this->_TempArr['CreateArr']				= 	array();
-			$this->_TempArr['CreateArr']['table_name'] 	= 	$prefix . 'info';
-			$this->_TempArr['CreateArr']['fields'] 		= 	array();
-			$this->_TempArr['CreateArr']['fields'][] 	= 	'id INT( 9 ) NOT NULL AUTO_INCREMENT PRIMARY KEY';
-			$this->_TempArr['CreateArr']['fields'][] 	= 	'var_name VARCHAR( 255 ) NOT NULL';
-			$this->_TempArr['CreateArr']['fields'][] 	= 	'value text NOT NULL';
-			
-			$create = $this->create_table($this->_TempArr['CreateArr']);
-			
-			if ($create)
-			{
-				$getoldinfo = sql_query('SELECT * FROM ' . $prefix . "oldinfo WHERE id='1'");
-				$old_info   = $MySmartBB->DB->sql_fetch_array($getoldinfo);
-				        
-		        $did = false;
-		        
-				$field_query = sql_query('SHOW FIELDS FROM ' . $prefix . 'oldinfo');
-				while ($row = $MySmartBB->DB->sql_fetch_array($field_query))
-				{
-					$value = addslashes($old_info[$row['Field']]);		    
-		    
-		    		$query = sql_query("INSERT INTO " . $prefix . "info(id,var_name,value) VALUES('NULL','" . $row['Field'] . "','" . $value . "')");
-		    		
-		    		if ($query)
-		    		{		    			
-		    			$did = true;
-		    		}
-				}
-
-				if ($did)
-				{			
-					$del = $this->drop_table($prefix . 'oldinfo');
-			
-					if ($del)
-					{
-						return true;
-					}
-				}
-			}
-		}
-	}
-	
 	// The create_date entry
 	function AddCreateDateEntry()
 	{
-		global $MySmartBB;
-		
-		$query = sql_query('SELECT register_date FROM ' . $MySmartBB->table['member'] . ' ORDER BY id ASC LIMIT 1');
-		$row = $MySmartBB->DB->sql_fetch_array($query);
-		
-		$date = $this->_DateFormatDo($row['register_date']);
-		
-		$new_date = explode('/', $date, 3);
-		$time = gmmktime(0,  0, 0, $new_date[1], $new_date[2], $new_date[0]);
-		
-		$insert = sql_query('INSERT INTO ' . $MySmartBB->table['info'] . " SET var_name='create_date',value='" . $time . "'");
-		
-		return ($insert) ? true : false;
-	}*/
-	
-	// Drop notinindex_id
-	function DropNotInIndex()
-	{
-		global $MySmartBB;
-		
-		$this->_TempArr['DropArr'] = array();
-		$this->_TempArr['DropArr']['table_name'] = $MySmartBB->table['online'];
-		$this->_TempArr['DropArr']['field_name'] = 'notinindex_id';
-		
-		$drop = $this->drop_field($this->_TempArr['DropArr']);
-		
-		return ($drop) ? true : false;
+
 	}
 	
-	// Add logged field
-	function AddLogged()
-	{
-		global $MySmartBB;
-		
-		$this->_TempArr['AddArr'] 				= array();
-		$this->_TempArr['AddArr']['table'] 		= $MySmartBB->table['member'];
-		$this->_TempArr['AddArr']['field_name'] = 'logged';
-		$this->_TempArr['AddArr']['field_des'] 	= 'VARCHAR( 30 ) NOT NULL';
-		
-		$add = $this->add_field($this->_TempArr['AddArr']);
-		
-		unset($this->_TempArr['AddArr']);
-		
-		return ($add) ? true : false;
-	}
+}
+
+// Original code By KHALED MAMDOUH (vbzoom.com)
+function convertDate( $date, $format = 'j/n/Y' )
+{
+	$day   = substr( $date, 8, 2);
+	$month = substr( $date, 5, 2);
+	$year  = substr( $date, 0, 4);
 	
-	function AddRegisterTime()
-	{
-		global $MySmartBB;
-		
-		$this->_TempArr['AddArr'] 				= array();
-		$this->_TempArr['AddArr']['table'] 		= $MySmartBB->table['member'];
-		$this->_TempArr['AddArr']['field_name'] = 'register_time';
-		$this->_TempArr['AddArr']['field_des'] 	= 'VARCHAR( 50 ) NOT NULL';
-		
-		$add = $this->add_field($this->_TempArr['AddArr']);
-		
-		return ($add) ? true : false;
-	}
+	return date( $format, mktime( 0, 0, 0, $month, $day, $year ) );	
+}
+
+function convertInfoTable()
+{
+	global $MySmartBB;
 	
-	//function NewRegisterDateFormat(/* I Love & :D */&$msgs)
-	/*{
-		global $MySmartBB;
-				
-		$query = sql_query('SELECT * FROM ' . $MySmartBB->table['member']);
+	$prefix = $MySmartBB->getPrefix();
+	
+	$rename = $MySmartBB->db->sql_query( "RENAME TABLE " . $prefix  . "info TO " . $prefix . "oldinfo" );
+	
+	if ( $rename )
+	{
+		// TODO :: The code of creating "info" table should be here
 		
-		while ($row = $MySmartBB->DB->sql_fetch_array($query))
+		/* $create = blah blah ...	*/
+		
+		if ( $create )
 		{
-			$r_date = $this->_DateFormatDo($row['register_date']);
+			$query = $MySmartBB->db->sql_query( 'SELECT * FROM ' . $prefix . "oldinfo WHERE id='1'" );
+			$old_info = $MySmartBB->db->sql_fetch_array( $query );
 			
-			$date = explode('/', $r_date, 3);
-			$time = gmmktime(0,  0, 0, $date[1], $date[2], $date[0]);
+			$did = false;
 			
-			$update = sql_query("UPDATE " . $MySmartBB->table['member'] . " SET register_time='" . $time . "'");
+			$query = $MySmartBB->db->sql_query( 'SHOW FIELDS FROM ' . $prefix . 'oldinfo' );
 			
-			if ($update)
+			while ( $r = $MySmartBB->db->sql_fetch_array($query) )
 			{
-				$msgs[] = 'تم تحديث تاريخ تسجيل ' . htmlspecialchars($row['username']);
+				$value = addslashes( $old_info[ $row['Field'] ] );
+				
+				$query = $MySmartBB->db->sql_query( "INSERT INTO " . $prefix . "info(id,var_name,value) VALUES('NULL','" . $r[ 'Field' ] . "','" . $value . "')" );
+				
+				$did = ( $query ) ? true : false;
+			}
+			
+			if ( $did )
+			{
+				$del = $this->drop_table($prefix . 'oldinfo');
+				
+				$del = $MySmartBB->db->sql_query( "DROP TABLE " . $prefix . "oldinfo" );
+				
+				return ( $del ) ? true : false;
+			}
+			else
+			{
+				return false;
 			}
 		}
-	}*/
-	
-	// By KHALED MAMDOUH .. vbzoom.com
- 	/*function _DateFormatDo($GetDateFormat,$DateFormat="j/n/Y")
-    {
-  		$Day   = SubStr($GetDateFormat,8,2);
-  		$Month = SubStr($GetDateFormat,5,2);
-  		$Year  = SubStr($GetDateFormat,0,4);
-
-  		$ResultDate = @date ($DateFormat, mktime (0,0,0,$Month,$Day,$Year));
-
-  		return $ResultDate;
- 	}*/
- 	
- 	/*function FixCaches(&$msgs)
- 	{
- 		global $MySmartBB;
- 		
- 		//  Smiles Cache
-		$update = $this->_UpdateSmileArray();
-		$msgs[] = ($update) ? 'تم تحديث كاش الابتسامات' : 'لم يتم تحديث كاش الابتسامات';
- 	}
- 		
-	function _UpdateSmileArray()
-	{
-		global $MySmartBB;
-		
-		$cache = $MySmartBB->icon->UpdateSmilesCache(null);
-		
-		return ($cache) ? true : false;
 	}
 	
-	function UpdateVersion()
-	{
-		global $MySmartBB;
-		
-		$update = sql_query("UPDATE " . $MySmartBB->table['info'] . " SET value='2.0 SEGMA' WHERE var_name='MySBB_version'");
-		
-		return ($update) ? true : false;
-	}*/
+	return false;
+}
+
+function storeCreateDate()
+{
+	global $MySmartBB;
+
+	$MySmartBB->rec->table = $MySmartBB->table[ 'member' ];
+	$MySmartBB->rec->order = 'id ASC';
+	$MySmartBB->rec->limit = '1';
+	
+	$row = $MySmartBB->rec->getInfo();
+	
+	$d = convertDate( $row[ 'register_date' ] );
+
+	$date = explode( '/', $d, 3 );
+	$time = gmmktime( 0, 0, 0, $date[ 1 ], $date[ 2 ], $date[ 0 ] );
+	
+	$MySmartBB->rec->table = $MySmartBB->table[ 'info' ];
+	$MySmartBB->rec->fields = array(	'var_name'	=>	'create_date',
+										'value'		=>	$time	);
+	
+	$insert = $MySmartBB->rec->insert();
+
+	return ( $insert ) ? true : false;
 }
 
 $Upgrader = new MySmartUpgrader( $MySmartBB, 'segma' );
-
-$Upgrader->addFields();
-
-
-
-$MySmartBB->install = new MySmartSEGMA;
 
 $MySmartBB->html->page_header('معالج ترقية برنامج منتديات MySmartBB');
 
@@ -236,34 +132,31 @@ elseif ($MySmartBB->_GET['step'] == 1)
 	$MySmartBB->html->cells('الخطوه الاولى : تعديلات على جداول قواعد البيانات','main1');
 	$MySmartBB->html->close_table();
 	
-	$p 			= 	array();
-	$msgs 		= 	$MySmartBB->install->_Masseges;
-	
-	/*$p[1] 		= 	$MySmartBB->install->ConvertInfoTable();
-	$msgs[1] 	= 	($p[1]) ? 'تم تحديث جدول المعلومات إلى النمط الجديد' : 'لم يتم تحديث جدول المعلومات إلى النمط الجديد';*/
-	
-	/*$p[2]		=	$MySmartBB->install->AddCreateDateEntry();
-	$msgs[2]	=	($p[2]) ? 'تم اضافه مدخل تاريخ انشاء المنتدى' : 'لم يتم اضافة مدخل تاريخ انشاء المنتدى';*/
-	
-	$p[3]		=	$MySmartBB->install->DropNotInIndex();
-	$msgs[3]	=	($p[3]) ? 'تم حذف الحقل' : 'لم يتم حذف الحقل';
-	
-	$p[4]		=	$MySmartBB->install->AddLogged();
-	$msgs[4]	=	($p[4]) ? 'تم اضافة الحقل logged' : 'لم يتم اضافة الحقل logged';
-	
-	$p[5]		=	$MySmartBB->install->AddRegisterTime();
-	$msgs[5]	=	($p[5]) ? 'تم اضافة الحقل register_time' : 'لم يتم اضافة الحقل register_time';
-	
 	$MySmartBB->html->open_p();
 	
-	foreach ($msgs as $msg)
+	if ( convertInfoTable() )
 	{
-		$MySmartBB->html->p_msg($msg);
+		$MySmartBB->html->p_msg( 'تم تحديث جدول المعلومات إلى النمط الجديد' );
 	}
+	else
+	{
+		$MySmartBB->html->p_msg( 'لم يتم تحديث جدول المعلومات إلى النمط الجديد' );
+	}
+	
+	if ( storeCreateDate() )
+	{
+		$MySmartBB->html->p_msg( 'تم اضافه مدخل تاريخ انشاء المنتدى' );
+	}
+	else
+	{
+		$MySmartBB->html->p_msg( 'لم يتم اضافه مدخل تاريخ انشاء المنتدى' );
+	}
+	
+	$Upgrader->addFields();
 	
 	$MySmartBB->html->close_p();
 	
-	$NewVersion = $MySmartBB->install->UpdateVersion();
+	$MySmartBB->info->updateInfo( 'MySBB_version', '2.0 SEGMA' );
 	
 	$MySmartBB->html->make_link('الخطوه الثانيه -> تعديل تاريخ التسجيل','?step=2');
 }
@@ -272,15 +165,29 @@ elseif ($MySmartBB->_GET['step'] == 2)
 	$MySmartBB->html->cells('الخطوه الثانيه : تعديل تاريخ التسجيل','main1');
 	$MySmartBB->html->close_table();
 	
-	$p		=	array();
-	
-	$p[]	=	$MySmartBB->install->NewRegisterDateFormat($MySmartBB->install->_Masseges);
-	
 	$MySmartBB->html->open_p();
 	
-	foreach ($MySmartBB->install->_Masseges as $msg)
+	$MySmartBB->rec->table = $MySmartBB->table[ 'member' ];
+	
+	$MySmartBB->rec->getList();
+	
+	while ( $r = $MySmartBB->rec->getInfo() )
 	{
-		$MySmartBB->html->p_msg($msg);
+		$d = convertDate( $r[ 'register_date' ] );
+		
+		$date = explode( '/', $d, 3 );
+		$time = gmmktime( 0, 0, 0, $date[ 1 ], $date[ 2 ], $date[ 0 ] );
+		
+		$MySmartBB->rec->table 		= 	$MySmartBB->table[ 'member' ];
+		$MySmartBB->rec->filter 	= 	"id='" . $r[ 'id' ] . "'";
+		$MySmartBB->rec->fields 	= 	array(	'register_time'	=>	$time	);
+		
+		$update = $MySmartBB->rec->update();
+		
+		if ( $update )
+		{
+			$MySmartBB->html->p_msg( 'تم تحديث تاريخ تسجيل ' . $r[ 'username' ] );
+		}
 	}
 	
 	$MySmartBB->html->close_p();
@@ -292,15 +199,17 @@ elseif ($MySmartBB->_GET['step'] == 3)
 	$MySmartBB->html->cells('الخطوه الثالثه : تعديل الكاش','main1');
 	$MySmartBB->html->close_table();
 	
-	$p		=	array();
-	
-	$p[]	=	$MySmartBB->install->FixCaches($MySmartBB->install->_Masseges);
-	
 	$MySmartBB->html->open_p();
 	
-	foreach ($MySmartBB->install->_Masseges as $msg)
+	$cache = $MySmartBB->icon->updateSmilesCache();
+	
+	if ( $cache )
 	{
-		$MySmartBB->html->p_msg($msg);
+		$MySmartBB->html->p_msg( 'تم تحديث كاش الابتسامات' );
+	}
+	else
+	{
+		$MySmartBB->html->p_msg( 'لم يتم تحديث كاش الابتسامات' );
 	}
 	
 	$MySmartBB->html->close_p();
