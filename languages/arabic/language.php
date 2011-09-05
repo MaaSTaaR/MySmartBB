4c3dc45d2ae99b173886dd22931258e4e732cb78
Mohammed Q. Hussain <MaaSTaaR@gmail.com>
2011-09-05 10:25:59 +0000

    The beginning of support i18n

---
+++ languages/arabic/language.php
@@ -0,0 +1,22 @@
+<?php
+
+$lang_info = array();
+$lang = array();
+$lang[ 'template' ] = array(); // Containts sentences inside the templates
+$lang[ 'common' ] = array(); // Common words
+
+// ... //
+
+// Information about the language files
+
+$lang_info[ 'direction' ]           = 'rtl';
+$lang_info[ 'name' ]                = 'اللغة العربية';
+$lang_info[ 'authors' ]             = array( 'MySmartBB Team' );
+$lang_info[ 'mysmartbb_version' ]   = '2.0.0 ALPHA 5';
+
+// ... //
+
+$lang[ 'common' ][ 'member' ] = 'عضو';
+$lang[ 'common' ][ 'visitor' ] = 'زائر';
+
+?>
---
+++ languages/arabic/main.lang.php
@@ -0,0 +1,14 @@
+<?php
+
+require_once( 'language.php' );
+
+$lang[ 'template' ][ 'main_page' ] = 'الصفحة الرئيسية';
+
+// Statistics table
+$lang[ 'template' ][ 'statistics' ]     = 'إحصائيات';
+$lang[ 'template' ][ 'online' ]         = 'المتواجدون الآن';
+$lang[ 'template' ][ 'no_online' ]      = 'لا يوجد شخص قام بتسجيل دخوله';
+$lang[ 'template' ][ 'today' ]          = 'من تواجد اليوم';
+$lang[ 'template' ][ 'no_today' ]       = 'لا يوجد عضو قام بزيارة المنتديات اليوم';
+
+?>
