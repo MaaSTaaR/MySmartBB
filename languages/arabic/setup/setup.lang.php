7d97defe6e350a24d9e0b208c0210371dbf69f7d
Mohammed Q. Hussain <MaaSTaaR@gmail.com>
2011-12-20 17:33:59 +0000

    Supporting i18n in installer

---
+++ languages/arabic/setup/setup.lang.php
@@ -0,0 +1,50 @@
+<?php
+
+$lang[ 'installation_wizard' ] = 'معالج تثبيت برنامج منتديات MySmartBB';
+$lang[ 'welcome_message_step' ] = 'اولاً : رسالة الترحيب';
+$lang[ 'welcome_message' ] = 'مرحباً بك، انت الآن مُقدم على تركيب الجيل الثاني من برنامج منتديات MySmartBB، من خلال هذا المعالج سوف يتم زرع الجداول المطلوبه و القيام بالاجراءات اللازمه حتى يعمل البرنامج،يرجى التحقق من انك قمت بالخطوات اللازمه ما قبل التثبيت،اذا كنت تحتاج للمساعده نرجوا منك مراجعة الموقع الرسمي.';
+$lang[ 'start_install' ] = 'إبدأ عملية التثبيت';
+$lang[ 'first_step' ] = 'الخطوه الاولى : التحقق من تصاريح المجلدات';
+$lang[ 'dir_permission' ] = 'تصريح المجلد';
+$lang[ 'uncorrect' ] =  'غير صحيح';
+$lang[ 'correct' ] = ' صحيح';
+$lang[ 'next_step' ] = 'الخطوه التاليه ->';
+$lang[ 'second_step' ] = 'الخطوه الثانيه :';
+$lang[ 'create_tables' ] = 'إنشاء الجداول';
+$lang[ 'third_step' ] = 'الخطوه الثالثه';
+$lang[ 'insert_data' ] = 'إدخال المعلومات';
+$lang[ 'fourth_step' ] = 'الخطوه الرابعه';
+$lang[ 'set_board_information' ] = 'إدخال معلومات المنتدى';
+$lang[ 'admin_info' ] = 'معلومات المدير';
+$lang[ 'username' ] = 'اسم المستخدم';
+$lang[ 'password' ] = 'كلمة المرور';
+$lang[ 'email' ] = 'البريد الالكتروني';
+$lang[ 'gender' ] = 'الجنس';
+$lang[ 'male' ] = 'ذكر';
+$lang[ 'female' ] = 'انثى';
+$lang[ 'board_information' ] = 'معلومات المنتدى';
+$lang[ 'board_title' ] = 'اسم المنتدى';
+$lang[ 'final_step' ] = 'الخطوه الاخيره';
+$lang[ 'fill_req_info' ] = 'يرجى تعبئة كافة المعلومات!';
+$lang[ 'admin' ] = 'المشرف العام';
+$lang[ 'admin_created' ] = 'تم إنشاء حساب المدير بنجاح';
+$lang[ 'admin_failed' ] = 'لم يتم إنشاء حساب المدير';
+$lang[ 'create_date_added' ] = 'تم تسجيل تاريخ إنشاء المنتدى';
+$lang[ 'create_date_failed' ] = 'لم يتم تسجيل تاريخ إنشاء المنتدى';
+$lang[ 'board_title_set' ] = 'تم وضع عنوان المنتدى';
+$lang[ 'board_title_failed' ] = 'لم يتم وضع عنوان المنتدى';
+$lang[ 'smile_cache_set' ] = 'تم وضع المعلومات المخبأه للابتسامات';
+$lang[ 'smile_cache_failed' ] = 'لم يتم وضع المعلومات المخبأه للابتسامات';
+$lang[ 'board_installed' ] = 'تهانينا! تم تثبيت المنتدى بنجاح. يرجى التأكد من حذفك لهذا المجلد';
+$lang[ 'goto_main_page' ] = 'للذهاب إلى الصفحه الرئيسيه للمنتدى :';
+$lang[ 'goto_admin_cp' ] = 'للذهاب إلى لوحة التحكم :';
+$lang[ 'here' ] = 'هنا';
+
+$lang[ 'file_doesnt_exist' ] = 'خطأ : الملف التالي غير موجود';
+$lang[ 'created' ] = 'تم إنشاء';
+$lang[ 'create_failed' ] = 'فشل في إنشاء';
+$lang[ 'error_in_tables_array' ] = 'هناك خطأ في مصفوفة الجداول';
+$lang[ 'process_succeed' ] = 'تمت العملية';
+$lang[ 'process_failed' ] = 'لم تتم العملية';
+
+?>
