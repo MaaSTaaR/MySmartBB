41d6a9d2be2df8dc4e217dda3323f35a9963e9d3
Mohammed Q. Hussain <MaaSTaaR@gmail.com>
2012-07-26 19:40:39 +0000

    Beta 3

--- includes/config.php
+++ includes/config.php
@@ -1,6 +1,6 @@
 <?php
 
-// MySmartBB 2.0.0 Beta 2
+// MySmartBB 2.0.0 Beta 3
 // http://www.MySmartBB.com
 
 
@@ -18,7 +18,7 @@ $config['db']['prefix']			=	'';				// MySmartBB_ if empty
 
 //=================
 
-$config[ 'lang_dir' ] = 'arabic'; // The language directory (Note : arabic and english are available now)
+$config[ 'lang_dir' ] = 'english'; // The language directory (Note : arabic and english are available now)
 
 
 ?>
