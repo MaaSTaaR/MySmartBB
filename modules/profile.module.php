18066413532c102e9464da4af75270af89396bc7
Mohammed Q. Hussain <MaaSTaaR@gmail.com>
2012-12-31 14:49:13 +0000

    Bugfixes, Codebase Improvements, Code Documantation

--- modules/profile.module.php
+++ modules/profile.module.php
@@ -1,12 +1,12 @@
 <?php
 
-(!defined('IN_MYSMARTBB')) ? die() : '';
+( !defined( 'IN_MYSMARTBB' ) ) ? die() : '';
 
-define('COMMON_FILE_PATH',dirname(__FILE__) . '/common.module.php');
+define( 'COMMON_FILE_PATH', dirname( __FILE__ ) . '/common.module.php' );
 
-include('common.php');
+include( 'common.php' );
 
-define('CLASS_NAME','MySmartProfileMOD');
+define( 'CLASS_NAME', 'MySmartProfileMOD' );
 
 class MySmartProfileMOD
 {
@@ -16,7 +16,7 @@ class MySmartProfileMOD
 		
 		$MySmartBB->loadLanguage( 'profile' );
 		
-		if ($MySmartBB->_GET['show'])
+		if ( $MySmartBB->_GET[ 'show' ] )
 		{
 			$this->_showProfile();
 		}
@@ -33,8 +33,6 @@ class MySmartProfileMOD
 		global $MySmartBB;
 		
 		// ... //
-		
-		// Show the header
 
 		$MySmartBB->func->showHeader( $MySmartBB->lang[ 'view_member_info' ] );
 		
@@ -46,30 +44,26 @@ class MySmartProfileMOD
 		
 		// ... //
 		
-		// To save some of server's resources when the member shows his/her profile
+		// To save some of server's resources when the member shows his/her own profile
 		// we don't execute a new query because we already have the information
 		// of the member stored in $MySmartBB->_CONF['member_row']
 		$do_query = true;
 		
-		if (!empty($MySmartBB->_GET['id']))
+		if ( !empty( $MySmartBB->_GET[ 'id' ] ) )
 		{
-			$id = (int) $MySmartBB->_GET['id'];
+			$id = (int) $MySmartBB->_GET[ 'id' ];
 			
 			$MySmartBB->rec->filter = "id='" . $id . "'";
 			
 			if ( $MySmartBB->_CONF[ 'member_permission' ] )
-			{
-				$do_query = ($MySmartBB->_CONF['member_row']['id'] == $MySmartBB->_GET['id']) ? false : true;
-			}
+				$do_query = ( $MySmartBB->_CONF[ 'member_row' ][ 'id' ] == $MySmartBB->_GET[ 'id' ] ) ? false : true;
 		}
-		elseif (!empty($MySmartBB->_GET['username']))
+		elseif ( !empty( $MySmartBB->_GET[ 'username' ] ) )
 		{
-			$MySmartBB->rec->filter = "username='" . $MySmartBB->_GET['username'] . "'";
+			$MySmartBB->rec->filter = "username='" . $MySmartBB->_GET[ 'username' ] . "'";
 			
 			if ( $MySmartBB->_CONF[ 'member_permission' ] )
-			{
-				$do_query = ($MySmartBB->_CONF['member_row']['username'] == $MySmartBB->_GET['username']) ? false : true;
-			}
+				$do_query = ( $MySmartBB->_CONF[ 'member_row' ][ 'username' ] == $MySmartBB->_GET[ 'username' ] ) ? false : true;
 		}
 		else
 		{
@@ -80,79 +74,84 @@ class MySmartProfileMOD
 		
 		$MySmartBB->rec->table = $MySmartBB->table[ 'member' ];
 		
-		$MySmartBB->_CONF['template']['MemberInfo'] = ($do_query) ? $MySmartBB->rec->getInfo() : $MySmartBB->_CONF['member_row'];
+		$MySmartBB->_CONF[ 'template' ][ 'MemberInfo' ] = ( $do_query ) ? $MySmartBB->rec->getInfo() : $MySmartBB->_CONF[ 'member_row' ];
 		
 		// ... //
 		
-		if (!$MySmartBB->_CONF['template']['MemberInfo'])
+		if ( !$MySmartBB->_CONF[ 'template' ][ 'MemberInfo' ] )
 			$MySmartBB->func->error( $MySmartBB->lang[ 'member_doesnt_exist' ] );
 		
 		// ... //
 		
 		// Where is the member now?
-     	$MySmartBB->online->updateMemberLocation( $MySmartBB->lang[ 'viewing_member_profile' ] . $MySmartBB->lang_common[ 'colon' ] . ' ' . $MySmartBB->_CONF['template']['MemberInfo']['username'] );
+     	$MySmartBB->online->updateMemberLocation( $MySmartBB->lang[ 'viewing_member_profile' ] . $MySmartBB->lang_common[ 'colon' ] . ' ' . $MySmartBB->_CONF[ 'template' ][ 'MemberInfo' ][ 'username' ] );
      	
      	// ... //
      	
-     	$MySmartBB->member->processMemberInfo( $MySmartBB->_CONF['template']['MemberInfo'] );
+     	$MySmartBB->member->processMemberInfo( $MySmartBB->_CONF[ 'template' ][ 'MemberInfo' ] );
 		
 		// ... //
 		
-		// If the member who we are showing his/her profile belong to the same
+		// If the member who we are showing his/her profile belongs to the same
 		// group of the current member (that is, the member who are showing the profile)
 		// don't do a query, we already have the information of the group stored in $MySmartBB->_CONF['group_info']
 		if ( $MySmartBB->_CONF[ 'member_permission' ] 
-			and $MySmartBB->_CONF['member_row']['usergroup'] == $MySmartBB->_CONF['template']['MemberInfo']['usergroup'])
+			and $MySmartBB->_CONF[ 'member_row' ][ 'usergroup' ] == $MySmartBB->_CONF[ 'template' ][ 'MemberInfo' ][ 'usergroup' ] )
 		{
-			$GroupInfo = $MySmartBB->_CONF['group_info'];
+			$GroupInfo = $MySmartBB->_CONF[ 'group_info' ];
 		}
 		else
 		{
 			$MySmartBB->rec->table = $MySmartBB->table[ 'group' ];
-			$MySmartBB->rec->filter = "id='" . $MySmartBB->_CONF['template']['MemberInfo']['usergroup'] . "'";
+			$MySmartBB->rec->filter = "id='" . $MySmartBB->_CONF[ 'template' ][ 'MemberInfo' ][ 'usergroup' ] . "'";
 			
 			$GroupInfo = $MySmartBB->rec->getInfo();
 		}
 			
-		$MySmartBB->_CONF['template']['MemberInfo']['usergroup'] = $GroupInfo['title'];
+		$MySmartBB->_CONF[ 'template' ][ 'MemberInfo' ][ 'usergroup' ] = $GroupInfo[ 'title' ];
 		
-		if ( $MySmartBB->_CONF['template']['MemberInfo']['posts'] > 0 )
+		if ( $MySmartBB->_CONF[ 'template' ][ 'MemberInfo' ][ 'posts' ] > 0 )
 		{
 			// ... //
 			
 			$MySmartBB->rec->table = $MySmartBB->table[ 'subject' ];
-			$MySmartBB->rec->filter = "writer='" . $MySmartBB->_CONF['template']['MemberInfo']['username'] . "' AND delete_topic<>'1' AND sec_subject<>'1'";
+			$MySmartBB->rec->filter = "writer='" . $MySmartBB->_CONF[ 'template' ][ 'MemberInfo' ][ 'username' ] . "' AND delete_topic<>'1' AND sec_subject<>'1'";
 			$MySmartBB->rec->order = "id DESC";
 			$MySmartBB->rec->limit = '0,1';
 			
-			$MySmartBB->_CONF['template']['LastSubject'] = $MySmartBB->rec->getInfo();
+			$MySmartBB->_CONF[ 'template' ][ 'LastSubject' ] = $MySmartBB->rec->getInfo();
 			
 			// ... //
 			
 			$MySmartBB->rec->table = $MySmartBB->table[ 'reply' ];
-			$MySmartBB->rec->filter = "writer='" . $MySmartBB->_CONF['template']['MemberInfo']['username'] . "' AND delete_topic<>'1'";
+			$MySmartBB->rec->filter = "writer='" . $MySmartBB->_CONF[ 'template' ][ 'MemberInfo' ][ 'username' ] . "' AND delete_topic<>'1'";
 			$MySmartBB->rec->order = "id DESC";
 			$MySmartBB->rec->limit = '1';
 			
 			$GetLastReplyInfo = $MySmartBB->rec->getInfo();
 		
-			if ($GetLastReplyInfo != false)
+			if ( $GetLastReplyInfo != false )
 			{
 				$MySmartBB->rec->table = $MySmartBB->table[ 'subject' ];
-				$MySmartBB->rec->filter = "id='" . $GetLastReplyInfo['subject_id'] . "'";
+				$MySmartBB->rec->filter = "id='" . $GetLastReplyInfo[ 'subject_id' ] . "'";
 				
-				$MySmartBB->_CONF['template']['LastReply'] = $MySmartBB->rec->getInfo();				
+				$MySmartBB->_CONF[ 'template' ][ 'LastReply' ] = $MySmartBB->rec->getInfo();				
 			}
 		}
 		
-		$MySmartBB->rec->table = $MySmartBB->table[ 'online' ];
-		$MySmartBB->rec->filter = "username='" . $MySmartBB->_CONF['template']['MemberInfo']['username'] . "'";
+		// ... //
 		
-		$MySmartBB->_CONF['template']['Location'] = $MySmartBB->rec->getInfo();
+		// TODO : For what?
+		$MySmartBB->rec->table = $MySmartBB->table[ 'online' ];
+		$MySmartBB->rec->filter = "username='" . $MySmartBB->_CONF[ 'template' ][ 'MemberInfo' ][ 'username' ] . "'";
+		
+		$MySmartBB->_CONF[ 'template' ][ 'Location' ] = $MySmartBB->rec->getInfo();
+		
+		// ... //
 		
 		$MySmartBB->plugin->runHooks( 'show_profile_end' );
 		
-		$MySmartBB->template->display('profile');
+		$MySmartBB->template->display( 'profile' );
 	}
 }
 	
