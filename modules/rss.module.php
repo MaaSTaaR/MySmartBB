64db395dbcd78a8c7bfba2b7fa339c7163bb11c6
Mohammed Q. Hussain <MaaSTaaR@gmail.com>
2013-01-01 10:03:01 +0000

    Bugfixes, Codebase Improvements, Code Documantation

--- modules/rss.module.php
+++ modules/rss.module.php
@@ -1,12 +1,12 @@
 <?php
 
-(!defined('IN_MYSMARTBB')) ? die() : '';
+( !defined( 'IN_MYSMARTBB' ) ) ? die() : '';
 
-define('COMMON_FILE_PATH',dirname(__FILE__) . '/common.module.php');
+define( 'COMMON_FILE_PATH', dirname( __FILE__ ) . '/common.module.php' );
 
-include('common.php');
+include( 'common.php' );
 
-define('CLASS_NAME','MySmartRSSMOD');
+define( 'CLASS_NAME', 'MySmartRSSMOD' );
 
 class MySmartRSSMOD
 {
@@ -19,15 +19,15 @@ class MySmartRSSMOD
 		echo '<?xml version="1.0"?>';
 		echo '<rss version="2.0">';
 		echo '<channel>';
-		echo '<title>' . $MySmartBB->_CONF['info_row']['title'] . '</title>';
+		echo '<title>' . $MySmartBB->_CONF[ 'info_row' ][ 'title' ] . '</title>';
 		echo '<link>' . $MySmartBB->func->getForumAdress() . '</link>';
-		echo '<description>' . $MySmartBB->lang[ 'latest_subjects_rss' ] . ' ' . $MySmartBB->_CONF['info_row']['title'] . '</description>';
+		echo '<description>' . $MySmartBB->lang[ 'latest_subjects_rss' ] . ' ' . $MySmartBB->_CONF[ 'info_row' ][ 'title' ] . '</description>';
 		
-		if ($MySmartBB->_GET['subject'])
+		if ( $MySmartBB->_GET[ 'subject' ] )
 		{
 			$this->_subjectRSS();
 		}
-		elseif ($MySmartBB->_GET['section'])
+		elseif ( $MySmartBB->_GET[ 'section' ] )
 		{
 			$this->_sectionRSS();
 		}
@@ -50,9 +50,9 @@ class MySmartRSSMOD
 		while ( $row = $MySmartBB->rec->getInfo() )
 		{
 			echo '<item>';
-			echo '<title>' . $row['title'] . '</title>';
-			echo '<link>' . $MySmartBB->func->getForumAdress() . 'index.php?page=topic&amp;show=1&amp;id=' . $row['id'] . '</link>';
-			echo '<description>' . $row['text'] . '</description>';
+			echo '<title>' . $row[ 'title' ] . '</title>';
+			echo '<link>' . $MySmartBB->func->getForumAdress() . 'index.php?page=topic&amp;show=1&amp;id=' . $row[ 'id' ] . '</link>';
+			echo '<description>' . $row[ 'text' ] . '</description>';
 			echo '</item>';
 		}
 	}
@@ -61,82 +61,77 @@ class MySmartRSSMOD
 	{
 		global $MySmartBB;
 		
-		// Clean id from any strings
-		$MySmartBB->_GET['id'] = (int) $MySmartBB->_GET['id'];
+		$MySmartBB->_GET[ 'id' ] = (int) $MySmartBB->_GET[ 'id' ];
 		
-		// No _GET['id'] , so ? show a small error :)
-		if (empty($MySmartBB->_GET['id']))
+		if ( empty( $MySmartBB->_GET[ 'id' ] ) )
 		{
 			echo '<item>';
 			echo '<title>' . $MySmartBB->lang_common[ 'wrong_path' ] . '</title>';
 			echo '</item>';
+			
+			return false;
 		}
-		else
-		{
-			$MySmartBB->rec->table = $MySmartBB->table[ 'section' ];
-			$MySmartBB->rec->filter = "id='" . $MySmartBB->_GET['id'] . "'";
-			
-			$Section = $MySmartBB->rec->getInfo();
-			
-			if (!$Section)
-			{
-				$MySmartBB->func->error( $MySmartBB->lang[ 'section_doesnt_exist' ] );
-			}	
 
-			/* ... */
-			
-			$MySmartBB->rec->table = $MySmartBB->table[ 'section_group' ];
-			$MySmartBB->rec->filter = "section_id='" . $Section['id'] . "' AND group_id='" . $MySmartBB->_CONF['group_info']['id'] . "'";
-			
-			// Ok :) , the permssion for this visitor/member in this section
-			$SectionGroup = $MySmartBB->rec->getInfo();
-			
-			/* ... */
-					
-			// This member can't view this section
-			if ($SectionGroup['view_section'] != 1)
-			{
-				echo '<item>';
-				echo '<title>' . $MySmartBB->lang[ 'cant_view_section' ] . '</title>';
-				echo '</item>';
+		$MySmartBB->rec->table = $MySmartBB->table[ 'section' ];
+		$MySmartBB->rec->filter = "id='" . $MySmartBB->_GET['id'] . "'";
+		
+		$Section = $MySmartBB->rec->getInfo();
+		
+		if ( !$Section )
+		{
+			echo '<item>';
+			echo '<title>' . $MySmartBB->lang[ 'section_doesnt_exist' ] . '</title>';
+			echo '</item>';
+		
+			return false;
+		}
+		
+		if ( $Section[ 'parent' ] == 0 )
+		{
+			echo '<item>';
+			echo '<title>' . $MySmartBB->lang[ 'this_is_main_section' ] . '</title>';
+			echo '</item>';
 				
-				return 0;
-			}
-			
-			// This is main section , so we can't get subjects list from it 
-			if ($Section['main_section'])
-			{
-				echo '<item>';
-				echo '<title>' . $MySmartBB->lang[ 'this_is_main_section' ] . '</title>';
-				echo '</item>';
+			return false;
+		}
+		
+		if ( !empty( $Section[ 'section_password' ] ) )
+		{
+			echo '<item>';
+			echo '<title>' . $MySmartBB->lang[ 'section_protected' ] . '</title>';
+			echo '</item>';
 				
-				return 0;
-			}
+			return false;
+		}
+		
+		$MySmartBB->rec->table = $MySmartBB->table[ 'section_group' ];
+		$MySmartBB->rec->filter = "section_id='" . $Section[ 'id' ] . "' AND group_id='" . $MySmartBB->_CONF[ 'group_info' ][ 'id' ] . "'";
+		
+		$SectionGroup = $MySmartBB->rec->getInfo();
+		
+		if ( !$SectionGroup[ 'view_section' ] )
+		{
+			echo '<item>';
+			echo '<title>' . $MySmartBB->lang[ 'cant_view_section' ] . '</title>';
+			echo '</item>';
 			
-			if (!empty($Section['section_password']))
-			{
-				echo '<item>';
-				echo '<title>' . $MySmartBB->lang[ 'section_protected' ] . '</title>';
-				echo '</item>';
-				
-				return 0;
-			}
-			
-			$MySmartBB->rec->table = $MySmartBB->table[ 'subject' ];
-			$MySmartBB->rec->filter = "section='" . $MySmartBB->_GET['id'] . "' AND delete_topic<>'1'";
-			$MySmartBB->rec->order = "write_time DESC";
-			$MySmartBB->rec->limit = '10';
-			
-			$MySmartBB->rec->getList();
-			
-			while ( $row = $MySmartBB->rec->getInfo() )
-			{
-				echo '<item>';
-				echo '<title>' . $row['title'] . '</title>';
-				echo '<link>' . $MySmartBB->func->getForumAdress() . 'index.php?page=topic&amp;show=1&amp;id=' . $row['id'] . '</link>';
-				echo '<description>' . $row['text'] . '</description>';
-				echo '</item>';			
-			}
+			return false;
+		}
+
+		$MySmartBB->rec->table = $MySmartBB->table[ 'subject' ];
+		$MySmartBB->rec->filter = "section='" . $MySmartBB->_GET[ 'id' ] . "' AND delete_topic<>'1'";
+		$MySmartBB->rec->order = "write_time DESC";
+		$MySmartBB->rec->limit = '10';
+		
+		$MySmartBB->rec->getList();
+		
+		while ( $row = $MySmartBB->rec->getInfo() )
+		{
+			echo '<item>';
+			echo '<title>' . $row[ 'title' ] . '</title>';
+			echo '<link>' . $MySmartBB->func->getForumAdress() . 'index.php?page=topic&amp;show=1&amp;id=' . $row[ 'id' ] . '</link>';
+			echo '<description>' . $row[ 'text' ] . '</description>';
+			echo '</item>';			
 		}
 	}
 }
