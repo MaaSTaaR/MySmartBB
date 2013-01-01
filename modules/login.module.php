ecd637c139bbe21f4b1ff8691130f02997263480
Mohammed Q. Hussain <MaaSTaaR@gmail.com>
2013-01-01 11:16:35 +0000

    Bugfixes, Codebase Improvements, Code Documantation

--- modules/login.module.php
+++ modules/login.module.php
@@ -24,7 +24,7 @@ class MySmartLoginMOD
 		{
 			$this->_startLogin();
 		}
-		// Login after register
+		// Login after registeration
 		elseif ( $MySmartBB->_GET[ 'register_login' ] )
 		{
 			$this->_startLogin( true );
@@ -41,89 +41,80 @@ class MySmartLoginMOD
 	{
 		global $MySmartBB;
 		
-		if ( !$register_login )
-		{
-			$username = trim( $MySmartBB->_POST[ 'username' ] );
-			$password = trim( md5( $MySmartBB->_POST[ 'password' ] ) );
-		}
-		else
-		{
-			$username = trim( $MySmartBB->_GET[ 'username' ] );
-			$password = trim( $MySmartBB->_GET[ 'password' ] );
-		}
+		// ... //
+		
+		$username = ( !$register_login ) ? $MySmartBB->_POST[ 'username' ] : $MySmartBB->_GET[ 'username' ];
+		$password = ( !$register_login ) ? md5( $MySmartBB->_POST[ 'password' ] ) : $MySmartBB->_GET[ 'password' ];
+		
+		$username = trim( $username );
+		$password = trim( $password );
+
+		// ... //
 		
 		if ( empty( $username ) or empty( $password ) )
 			$MySmartBB->func->error( $MySmartBB->lang_common[ 'please_fill_information' ] );
 		
-		$expire = ( isset( $MySmartBB->_POST[ 'temporary' ] ) and $MySmartBB->_POST[ 'temporary' ] == 'on' ) ? 0 : time() + 31536000;
+		$expire = ( $MySmartBB->_POST[ 'temporary' ] == 'on' ) ? 0 : time() + 31536000;
+		
+		// ... //
 		
 		$isMember = $MySmartBB->member->loginMember( $username, $password, $expire );
 		
 		$MySmartBB->func->showHeader( $MySmartBB->lang[ 'login' ] );
 		
-		if ( $isMember != false )
+		if ( !$isMember )
 		{
-			// ... //
+			$MySmartBB->plugin->runHooks( 'login_failed' );
 			
-			$MySmartBB->template->assign( 'username', $username );
-			
-			$MySmartBB->template->display( 'login_msg' );
-			
-			// ... //
-			
-			if ( $isMember[ 'style' ] != $isMember[ 'style_id_cache' ] )
-			{
-				$MySmartBB->rec->filter = "id='" . (int) $isMember[ 'style' ] . "'";
-				
-				$style_cache = $MySmartBB->style->createStyleCache( $isMember['style'] );
-				
-				$MySmartBB->rec->table = $MySmartBB->table[ 'member' ];
-				$MySmartBB->rec->fields = array( 	'style_cache'	=>	$style_cache,
-													'style_id_cache'	=>	$isMember['style']	);
-				
-				$MySmartBB->rec->filter = "id='" . (int) $isMember['id'] . "'";
-				
-				$update_cache = $MySmartBB->rec->update();
-			}
-			
-			// ... //
-			
-			$MySmartBB->rec->table = $MySmartBB->table[ 'online' ];
-			$MySmartBB->rec->filter = "user_ip='" . $MySmartBB->_CONF['ip'] . "'";
-			
-			$MySmartBB->rec->delete();
-			
-			// ... //
-			
-			$MySmartBB->plugin->runHooks( 'login_success' );
-			
-       		// ... //
-      		
-      		$move_to = 'index.php';
-      		
-      		if ( !$register_login )
-      		{
-				$url = parse_url( $MySmartBB->_SERVER[ 'HTTP_REFERER' ] );
-		  		$url = $url[ 'query' ];
-		  		$url = explode( '&', $url );
-		  		$url = $url[ 0 ];
-
-		 		$Y_url = explode( '/', $MySmartBB->_SERVER[ 'HTTP_REFERER' ] );
-		  		$X_url = explode( '/', $MySmartBB->_SERVER[ 'HTTP_HOST' ] );
-		  		
-      			if ( $X_url[ 0 ] == $Y_url[ 2 ] )
-      				if ( $url != 'page=logout' or empty( $url ) or $url != 'page=login' )
-       					$move_to = $MySmartBB->_SERVER[ 'HTTP_REFERER' ];
-      		}
-      		
-      		$MySmartBB->func->move( $move_to );
+			$MySmartBB->func->error( $MySmartBB->lang[ 'login_failed' ] );
 		}
-		else
-		{
-		    $MySmartBB->plugin->runHooks( 'login_failed' );
-		    
-			$MySmartBB->func->msg( $MySmartBB->lang[ 'login_failed' ] );
-		}
+		
+		// ... //
+		
+		$MySmartBB->template->assign( 'username', $username );
+		$MySmartBB->template->display( 'login_msg' );
+		
+		// ... //
+		
+		if ( $isMember[ 'style' ] != $isMember[ 'style_id_cache' ] )			
+			$MySmartBB->member->updateMemberStyleCache( $isMember[ 'style' ], $isMember );
+		
+		// ... //
+		
+		$MySmartBB->rec->table = $MySmartBB->table[ 'online' ];
+		$MySmartBB->rec->filter = "user_ip='" . $MySmartBB->_CONF[' ip' ] . "'";
+		
+		$MySmartBB->rec->delete();
+		
+		// ... //
+		
+		$MySmartBB->plugin->runHooks( 'login_success' );
+		
+       	// ... //
+      	
+      	$move_to = ( !$register_login ) ? $this->_getMoveLocation() : 'index.php';
+      	$MySmartBB->func->move( $move_to );
+	}
+	
+	private function _getMoveLocation()
+	{
+		global $MySmartBB;
+		
+		$move_to = 'index.php';
+		
+		$url = parse_url( $MySmartBB->_SERVER[ 'HTTP_REFERER' ] );
+		$url = $url[ 'query' ];
+		$url = explode( '&', $url );
+		$url = $url[ 0 ];
+		
+		$Y_url = explode( '/', $MySmartBB->_SERVER[ 'HTTP_REFERER' ] );
+		$X_url = explode( '/', $MySmartBB->_SERVER[ 'HTTP_HOST' ] );
+		
+		if ( $X_url[ 0 ] == $Y_url[ 2 ] )
+			if ( $url != 'page=logout' or empty( $url ) or $url != 'page=login' )
+				$move_to = $MySmartBB->_SERVER[ 'HTTP_REFERER' ];
+		
+		return $move_to;
 	}
 }
 
